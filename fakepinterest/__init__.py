3b5584580c52d2b5fb9620789fc289b27198c0ff
scalzin <luanacunha.k@gmail.com>
2023-10-05 17:22:39 -0300

    mudanca banco dados

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -2,10 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+import os
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 app.config['SECRET_KEY'] = 'ae201be0f4bf54b1837088a1038ad4d8'
 app.config['UPLOAD_FOLDER'] = 'static/fotos_posts'
 
