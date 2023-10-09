de7d54c0703b4da8277e09385a462fcf1079bc42
scalzin <luanacunha.k@gmail.com>
2023-10-09 16:29:40 -0300

    att bd

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -6,7 +6,7 @@ import os
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://banco_fakepinterest_kgwa_user:SbjmTUBjEN6wFjaTnwl255JOuQsi5Nnt@dpg-cki4h1uafg7c73elpjo0-a/banco_fakepinterest_kgwa"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://banco_fakepinterest_kgwa_user:SbjmTUBjEN6wFjaTnwl255JOuQsi5Nnt@dpg-cki4h1uafg7c73elpjo0-a/banco_fakepinterest_kgwa"
 app.config['SECRET_KEY'] = 'ae201be0f4bf54b1837088a1038ad4d8'
 app.config['UPLOAD_FOLDER'] = 'static/fotos_posts'
 
